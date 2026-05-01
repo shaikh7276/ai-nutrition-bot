34912fcf71d7be1bc9ea8494c94522a231c1715b
shaikh7276 <ksk475225@gmail.com>
2026-05-01 17:09:25 +0530

    Fix model initialization in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ genai.configure(api_key=os.getenv("AIzaSyAb5s0dTp12BOOqtYaHEscLMJ1_b0KLKQE"))
 
 for m in genai.list_models():
     print(m.name, m.supported_generation_methods)
-    model = genai.GenerativeModel("model="gemini-1.0-pro"")
+    model = genai.GenerativeModel("gemini-1.0-pro")
     import streamlit as st
 import google.generativeai as genai
 import os
